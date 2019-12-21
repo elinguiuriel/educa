030d1ddc42bc4886ac92bb67e9713e4b78bff998
elinguiuriel <elinguiuriel@gmail.com>
2019-12-21 16:52:59 +0000

    Adding ordering to module and content objects

--- app/courses/models.py
+++ app/courses/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from .fields import OrderField
+
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -35,20 +37,29 @@ class Module(models.Model):
     course = models.ForeignKey(Course, related_name='modules', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
-        return self.title
+        return '{}. {}'.format(self.order, self.title)
+
+    class Meta:
+        ordering = ['order']
 
 
 class Content(models.Model):
     module = models.ForeignKey(Module, related_name='contents',
         on_delete=models.CASCADE)
-    content_type = models.ForeignKey(
-        ContentType, on_delete=models.CASCADE,
-        limit_choices_to={'model__in':('text', 'video', 'image','file')}
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in':('text', 'video', 'image','file')
+        }
     )
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
--- app/courses/tests/test_models.py
+++ app/courses/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from ..models import Subject, Course, Module
 from django.db.utils import DataError
-
+from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
@@ -27,6 +27,38 @@ class ModelTests(TestCase):
         subject = Subject()
         subject.title = title
         
-        with self.assertRaisesMessage(DataError, 'value too long for type character varying(200)'):
+        with self.assertRaisesMessage(DataError, 
+            'value too long for type character varying(200)'):
             subject.save()
 
+    def test_module_ordering_successful(self):
+        """Test that created module will be well ordered by subject"""
+
+        user = get_user_model().objects.create_user(
+            username='user',
+            email='user@uriellabs.com',
+            password='Testpass123'
+        )
+
+        subject = Subject()
+        subject.title = 'Mathematics'
+        subject.save()
+
+        c1 = Course.objects.create(subject=subject, owner=user, 
+            title='Course 1', slug='course1')
+        c2 = Course.objects.create(subject=subject, 
+            title='Course 2', slug='course2', owner=user)
+
+        # module of course 1
+        m1 = Module.objects.create(course=c1, title='Module 1')
+        m2 = Module.objects.create(course=c1, title='Module 2')
+        m3 = Module.objects.create(course=c1, title='Module 3', order=5)
+        m4 = Module.objects.create(course=c1, title='Module 4')
+        # first module of course 2
+        m5 = Module.objects.create(course=c2, title='Module 1')        
+
+        self.assertEqual(m1.order, 0)
+        self.assertEqual(m2.order, 1)
+        self.assertEqual(m3.order, 5)
+        self.assertEqual(m4.order, 6)
+        self.assertEqual(m5.order, 0)
