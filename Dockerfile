bb6b50c055efca219f37661944c7b38b0291c5bb
elinguiuriel <elinguiuriel@gmail.com>
2019-12-21 11:11:34 +0000

    Adding more variable in .env.dev
    For a better parametrization of the application when I will go to
    production

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM python:3.7-alpine
-LABEL AUTHOR="ELINGUI Pascal Uriel"
+LABEL MAINTAINER="ELINGUI Pascal Uriel"
 
+# set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
--- app/educa/settings.py
+++ app/educa/settings.py
@@ -73,15 +73,15 @@ WSGI_APPLICATION = 'educa.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
